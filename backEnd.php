42bcd84c5b4bab87eba9ad1bec4a9d200f312c1a
Michael <mchen628@gmail.com>
2017-11-29 22:03:18 -0500

    Added some databases related files

---
+++ backEnd.php
@@ -0,0 +1,39 @@
+<?php
+    declare(strict_types=1);
+    class item {
+        private $name, $price, $link;        
+        function _constructor(string $name, double $price, string $link){
+            $this->name = $name;
+            $this->price = $price;
+            $this->link = $link;
+        }
+        function getName(){
+            
+            return $this->name;
+        }
+        function getPrice(){
+            return $this->price;
+        }
+        function getLink(){
+            return $this->link;
+        }
+        function setName($name){
+            $this->name =$name;
+            
+        }
+        function setPrice($price){
+            $this->price = $price;
+        }
+        function setLink($link){
+            $this->link = $link;
+        }
+        
+    }
+
+    
+
+
+
+
+
+?>
